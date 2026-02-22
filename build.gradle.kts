0d3fbe01e997e9a944b85777babdb82863a30350
Kyungtae Park <pktpkt8917@gmail.com>
2026-02-22 19:36:34 +0900

    test : webflux 테스트 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("tools.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("org.springframework.boot:spring-boot-webtestclient")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- src/test/kotlin/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.kt
+++ src/test/kotlin/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.kt
@@ -1,13 +1,34 @@
 package com.polarbookshop.catalog_service
 
+import com.polarbookshop.catalog_service.domain.Book
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
-class CatalogServiceApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class CatalogServiceApplicationTests(
+    @Autowired private val webTestClient: WebTestClient
+) {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun `when post request then book created`() {
+        val expectedBook = Book("1231231231", "Title", "Author", 9.90)
+        webTestClient.post()
+            .uri("/books")
+            .bodyValue(expectedBook)
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isCreated
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectBody(Book::class.java)
+            .value { actualBook ->
+                assertThat(actualBook).isNotNull
+                assertThat(actualBook!!.isbn).isEqualTo(expectedBook.isbn)
+            }
+    }
 }
